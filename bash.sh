22675cd220064e8aa2084ab55592ca96cbda540b
root <root@01163-1-1503834.(none)>
2013-02-26 12:10:11 -0800

    modify bash to add lorem on every run

--- bash.sh
+++ bash.sh
@@ -2,15 +2,17 @@
 
 list=`ls -d submissions/*/`
 
-
 for D in $list
 do
     rm ../html/$1/script.js
     rm ../html/$1/savePage.php
-    #cp -a $D. ../html/$1/
-    
-    cp $D/script.js ../html/$1/
-    cp $D/savePage.php ../html/$1/
+   
+    #default savePage (in case student didn't submit one)
+    cp ../html/savePage.php ../html/$1/savePage.php
+ 
+    cp $D/script.js ../html/$1/script.js
+    cp $D/savePage.php ../html/$1/savePage.php
+    cp ../html/lorem.txt ../html/$1/lorem.txt
 
     node test.js $2/$1/index.php chrome $D
 done
